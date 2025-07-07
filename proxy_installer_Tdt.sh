761b5196c59db76fcaf7bf4ab545c8ae3c577709
hoangdinh9999 <vanheoshinglq@gmail.com>
2025-07-07 14:55:49 +0700

    Add files via upload

---
+++ proxy_installer_Tdt.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+choice=4
+config_mode=2
+
+export MANUAL_USERNAME="Tdt12345"
+export MANUAL_PASSWORD="Tdt12345"
+export MANUAL_HTTP_USERNAME="Tdt12345"
+export MANUAL_HTTP_PASSWORD="Tdt12345"
+export MANUAL_PORT=1080
+export MANUAL_HTTP_PORT=3128
+
+export DEBIAN_FRONTEND=noninteractive
+
+curl -sSL https://raw.githubusercontent.com/hoangdinh-dev/proxy-auto/main/auto_proxy_installer.sh | bash -s -- "$choice" "$config_mode"
