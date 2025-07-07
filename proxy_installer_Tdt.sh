3b27e3220178623392e3036d0bf5bb0818758c6f
hoangdinh9999 <vanheoshinglq@gmail.com>
2025-07-07 15:08:39 +0700

    proxy_installer_Tdt.sh

--- proxy_installer_Tdt.sh
+++ proxy_installer_Tdt.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 set -euo pipefail
 
 choice=4
@@ -14,4 +13,4 @@ export MANUAL_HTTP_PORT=3128
 
 export DEBIAN_FRONTEND=noninteractive
 
-curl -sSL https://raw.githubusercontent.com/hoangdinh-dev/proxy-auto/main/auto_proxy_installer.sh | bash -s -- "$choice" "$config_mode"
+curl -sSL https://raw.githubusercontent.com/hoangdinh9999/proxy-gcp/main/auto_proxy_installer.sh | bash -s -- "$choice" "$config_mode"
